04e3c36237f22d149bdbb72d846f184b4aff85de
sangamsharma1921 <sangamsharma1921@gmail.com>
2026-03-10 20:17:25 +0530

    This is my second change

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let celsius;
 let fahrenheit;
 document.getElementById("submit").onclick= function() {
-    celsius = document.getElementById("temp").value;
+    celsius = document.getElementById("temps").value;
     celsius = Number(celsius);
     fahrenheit = (celsius * 1.8)+32;
     document.getElementById("myH3").textContent = `${fahrenheit} degree Fahrenheit`;
